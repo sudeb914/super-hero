b5172628f285803d25cf3b5b9bab9d7970e4dfde
unknown <sudebkarmokar8@gmail.com>
2021-09-26 14:46:45 +0600

    edit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,11 +10,12 @@ const Cart = (props) => {
     return (
         <div>
             <h3>Cart Quantity : {props.cart.length}</h3>
-            <p>Name: 
-            {
-                cart.map(singleProduct => singleProduct.name)
-            }
-            </p>
+            <h4>Selected Item</h4>
+            <ul>
+                {
+                    cart.map((item, index)=><li key = {index} className="list-group-item">{item.name}</li>)
+                }
+            </ul>
             <p>Total : {total}</p>
             <button className='add-to-cart-btn'>Read More</button>
             
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 
 import './Item.css';
 
 const Item = (props) => {
     // console.log(props);
-    const {name,img,address,profession,salary} = props.item;
+    const {name,img,age,address,profession,salary} = props.item;
     const element = <FontAwesomeIcon icon={faShoppingCart} />
 
     return (
@@ -17,6 +17,7 @@ const Item = (props) => {
            <img className='img-size' src={img} alt="" />
             <h4>Name : {name}</h4>
             <p>Profession : {profession}</p>
+            <p>age : {age}</p>
             <p>Address : {address}</p>
             <p>Salary : {salary}</p>
             <button onClick={() => props.handleAddtocart(props.item)} className='add-to-cart-btn'>{element} Add to Cart</button>
